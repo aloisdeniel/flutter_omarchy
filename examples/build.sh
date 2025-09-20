439c684061bfb472db95f866357e81cdb776da67
Aloïs DENIEL <alois.deniel@gmail.com>
2025-09-20 23:04:14 +0200

    Cleaned pubspecs and packages.

--- examples/build.sh
+++ examples/build.sh
@@ -3,4 +3,4 @@ rm -rf ./app
 #dart pub global activate mason_cli
 #mason make omarchy_app -o ./app --project_name app --executable_name app --description "A simple Omarchy application." --author "Your Name" --config_path="\$HOME/.config/\$PROJECT_NAME/config.yaml"
 
-dart ../packages/flutter_omarchy_cli/bin/flutter_omarchy_cli.dart create --project_name app --executable_name app --description "A simple Omarchy application." --author "Your Name"  ./app
+dart ../packages/flutter_omarchy_cli/bin/flutter_omarchy_cli.dart create app --project_name app --executable_name app --description "A simple Omarchy application." --author "Your Name"  ./app
