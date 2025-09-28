c0cf9148be0c4a8a9257ee16d92ad044668c4198
aloisdeniel <alois.deniel@outlook.com>
2025-09-28 20:33:28 +0000

    Deploying to gh-pages from @ aloisdeniel/flutter_omarchy@19155d8b33c2befb05d460fa352e7eb652b3e554 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,7 +48,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.wasm": "fb92a23df7f6a5ab2969881d9b5feeb7",
-"flutter_bootstrap.js": "7ce9301f87bf4a42bbf60a94e6ad27ce",
+"flutter_bootstrap.js": "4f40e7806f81ed17115c631d2be3f8e2",
 "version.json": "ff966ab969ba381b900e61629bfb9789",
 "main.dart.js": "8cb812b98245c42a9a83516cd34d91db"};
 // The application shell files that are downloaded before a service worker can
